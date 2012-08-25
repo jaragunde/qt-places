ba731fed332773164998a7dc0b6a5fd3ed257372
Jacobo Aragunde Pérez <jaragunde@igalia.com>
2012-08-25 19:23:51 +0200

    Added missing file util.js.
    Previous patches about DB save/load don't work without it!

---
+++ qml/Places/util.js
@@ -0,0 +1,7 @@
+function Place(code, name, description, lat, lng) {
+    this.code = code
+    this.name = name
+    this.description = description
+    this.latitude = lat
+    this.longitude = lng
+}
